226d6f228a1dc2916de998fc78a1cb1de8962364
Peter Jang <peter.x.jang@gmail.com>
2019-04-07 08:14:43 -0500

    Add photos index action

---
+++ src/views/PhotosIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>All Photos</h1>
+    <div v-for="photo in photos">
+      <h2>{{ photo.name }}</h2>
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      photos: []
+    };
+  },
+  created: function() {
+    axios.get("/api/photos").then(response => {
+      this.photos = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
